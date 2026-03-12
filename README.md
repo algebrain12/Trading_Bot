cc42e8c08dd45cfb235caa2580133eaa5c9a4680
algebrain12 <print.akshan.sharma@gmail.com>
2026-03-12 10:43:14 +0530

    Updated with working intraday API

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 
 alpha-vantage = CVY7POQY7988Y1NJ
 finnhub = d6oq9fpr01qi5kh3uh4gd6oq9fpr01qi5kh3uh50
+
+
+Libraries required
+yfinance
+pandas
+numpy
+matplotlib
--- main.py
+++ main.py
@@ -1,19 +1,11 @@
-import finnhub
 import pandas as pd
 import matplotlib.pyplot as plt
 import time
+import yfinance as yf
 
-finnhub_client = finnhub.Client(api_key="d6oq9fpr01qi5kh3uh4gd6oq9fpr01qi5kh3uh50")
+def get_nifty50_data():
+    nifty50 = yf.download("^NSEI", period="10d", interval="1h")
+    nifty50.to_csv("nifty50_data.csv")
+    return nifty50 
 
-quote = finnhub_client.quote('AAPL')
-print("Real-time Quote for AAPL:")
-print(quote)
-
-now = int(time.time())
-one_month_ago = now - 30*24*60*60
-
-now = int(time.time())
-seven_days_ago = now - 7*24*60*60
-
-# Step 3: Get hourly candles (resolution = 60 minutes)
-candles = finnhub_client.stock_candles('AAPL', '60', seven_days_ago, now)
+print(get_nifty50_data())
